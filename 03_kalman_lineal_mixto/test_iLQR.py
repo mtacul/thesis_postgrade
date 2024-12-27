6947ee1f16da56d44d49c1de7224682a9f5e559b
mtacul <nachitotv2000@gmail.com>
2024-12-27 08:46:47 -0300

    lqr test

--- 03_kalman_lineal_mixto/test_iLQR.py
+++ 03_kalman_lineal_mixto/test_iLQR.py
@@ -9,13 +9,13 @@ import functions_03_rw
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.linalg import solve_discrete_are
-import control
+from scipy.linalg import solve_discrete_are, solve_continuous_are
+import control as ctrl
 
 
 deltat = 2
 # limite =  5762*69
-limite =  5762*3
+limite =  5762*1
 t = np.arange(0, limite, deltat)
 
 #%% Parámetros geométricos y orbitales dados
@@ -70,102 +70,227 @@ s_body_i = [0.776906,
 -0.249251]
 
 
-A_discretee = np.array([
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, -1.38567e-10, 0, 1, 0.000422084, 0, -2.12526e-07, -0.000137263, 0, 0, 0, 0, 0, 0],
-    [0, -1.9842e-09, 1, 0, -0.00151601, 1, 0, -0.00056993, 8.6575e-08, 0, 0, 0, 0, 0, 0],
-    [-9.56484e-07, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 6.88725e-06, -4.15701e-10, 0, 1, 0.000844169, 0, -6.37578e-07, -0.000274527, 0, 0, 0, 0, 0, 0],
-    [0, -5.95261e-09, -7.43188e-07, 0, -0.00303203, 0.999999, 0, -0.00113986, 2.59725e-07, 0, 0, 0, 0, 0, 0],
-    [1.15136e-21, 0, 0, -4.82914e-15, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, -7.87562e-06, 4.15701e-10, 0, -2.24185e-06, -0.000844169, 0, 1, 0.000274527, 0, 0, 0, 0, 0, 0],
-    [0, 5.95261e-09, 7.43188e-07, 0, 0.00303203, 1.17025e-06, 0, 0.00113986, 1, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 2, -6.92835e-11, 0, 1, 0.00028139, 0, -1.06263e-07, -9.15088e-05, 0, 1, 0, 0, 0, 0],
-    [0, -9.92102e-10, 2, 0, -0.00101068, 1, 0, -0.000379953, 4.32875e-08, 0, 0, 1, 0, 0, 0],
-    [-9.56484e-07, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 6.88725e-06, -2.77134e-10, 0, 2, 0.000844169, 0, -4.25052e-07, -0.000274526, 0, 0, 0, 0, 1, 0],
-    [0, -3.96841e-09, -7.43189e-07, 0, -0.00303203, 2, 0, -0.00113986, 1.7315e-07, 0, 0, 0, 0, 0, 1]
-])
-
-B_discretee = np.array([
-    [20.6186, 0, 0],
-    [0, 21.0526, 0.00319961],
-    [0, -0.089269, 69.9301],
-    [41.2371, 0, 0],
-    [0, 42.1052, 0.00959882],
-    [0, -0.267807, 139.86],
-    [358.763, 0, 0],
-    [0, 357.895, -0.00959882],
-    [0, 0.267807, 360.14],
-    [13.7457, 0, 0],
-    [0, 14.0351, 0.0015998],
-    [0, -0.0446345, 46.62],
-    [41.2371, 0, 0],
-    [0, 42.1052, 0.00639921],
-    [0, -0.178538, 139.86]
-])
-
-Aa = np.array([
-    [0, 0, -0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [-0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, -0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [-4.78242e-07, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 3.44362e-06, 0, 0, 0, 0.000422084, 0, 0, -0.000137263, 0, 0, 0, 0, 0, 0],
-    [0, 0, -3.71594e-07, 0, -0.00151601, 0, 0, -0.00056993, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, -0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, -3.93781e-06, 0, 0, 0, -0.000422084, 0, 0, 0.000137263, 0, 0, 0, 0, 0, 0],
-    [0, 0, 3.71594e-07, 0, 0.00151601, 0, 0, 0.00056993, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-])
-
-Ba = np.array([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [20.6186, 0, 0],
-    [0, 21.0526, 0],
-    [0, 0, 69.9301],
-    [179.381, 0, 0],
-    [0, 178.947, 0],
-    [0, 0, 180.07],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-])
-
 [A,B,A_discrete,B_discrete] = functions_03_rw.A_B(I_x,I_y,I_z,w0_O,0,0,0 , I_s0_x, I_s1_y, I_s2_z, 0,0,0, J_x, J_y, J_z, deltat, hh, bi_orbit,b_body_i, s_body_i)
 
-# asd
-#%% Control LQR
+# # Índices correspondientes a las filas y columnas
+# indices_0 = [0, 3, 6]
+# indices_1 = [1, 4, 7]
+# indices_2 = [2, 5, 8]
+
+# # Seleccionar la primera columna y las filas deseadas
+# B_discrete_3x1_0 = B_discrete[indices_0, 0]
+# B_discrete_3x1_1 = B_discrete[indices_1, 1]
+# B_discrete_3x1_2 = B_discrete[indices_2, 2]
+
+# # Convertir a una matriz columna (3x1)
+# B_discrete_0 = B_discrete_3x1_0.reshape(-1, 1)# Extraer submatriz de 3x3
+# B_discrete_1 = B_discrete_3x1_1.reshape(-1, 1)# Extraer submatriz de 3x3
+# B_discrete_2 = B_discrete_3x1_2.reshape(-1, 1)# Extraer submatriz de 3x3
+
+# A_discrete_0 = A_discrete[np.ix_(indices_0, indices_0)]
+# A_discrete_1 = A_discrete[np.ix_(indices_1, indices_1)]
+# A_discrete_2 = A_discrete[np.ix_(indices_2, indices_2)]
+
+C = np.eye(3)
+D = np.zeros((3, 1))  # Assuming D has the same number of rows as A and the same number of columns as B
+
+# Índices correspondientes a las filas y columnas
+indices_0 = [0, 3, 6]
+indices_1 = [1, 4, 7]
+indices_2 = [2, 5, 8]
+
+# Seleccionar la primera columna y las filas deseadas
+B_3x1_0 = B[indices_0, 0]
+B_3x1_1 = B[indices_1, 1]
+B_3x1_2 = B[indices_2, 2]
+
+# Convertir a una matriz columna (3x1)
+B_0 = B_3x1_0.reshape(-1, 1)# Extraer submatriz de 3x3
+B_1 = B_3x1_1.reshape(-1, 1)# Extraer submatriz de 3x3
+B_2 = B_3x1_2.reshape(-1, 1)# Extraer submatriz de 3x3
+
+A_0 = A[np.ix_(indices_0, indices_0)]
+A_1 = A[np.ix_(indices_1, indices_1)]
+A_2 = A[np.ix_(indices_2, indices_2)]
+
+
+
+
+# sys_continuous_0 = ctrl.StateSpace(A_3x3_0, B_3x1_0, C, D)
+# sys_continuous_1 = ctrl.StateSpace(A_3x3_1, B_3x1_1, C, D)
+# sys_continuous_2 = ctrl.StateSpace(A_3x3_2, B_3x1_2, C, D)
+
+# # A_discrete, B_discrete, _, _, _ = cont2discrete((Aa, Ba, Ca, Da), deltat, method='zoh')
+
+# # Discretize the system
+# sys_discrete_0 = ctrl.c2d(sys_continuous_0, hh, method='zoh')
+# sys_discrete_1 = ctrl.c2d(sys_continuous_1, hh, method='zoh')
+# sys_discrete_2 = ctrl.c2d(sys_continuous_2, hh, method='zoh')
+
+# # Extract the discretized A and B matrices
+# A_discrete_0 = sys_discrete_0.A
+# A_discrete_1 = sys_discrete_1.A
+# A_discrete_2 = sys_discrete_2.A
+
+# B_discrete_0 = sys_discrete_0.B
+# B_discrete_1 = sys_discrete_1.B
+# B_discrete_2 = sys_discrete_2.B
+
+
+#%% Control LQR en cada matriz
 
 # Definir las matrices Q y R del coste del LQR
-diag_Q = np.array([10, 10, 10, 0.1, 0.1, 0.1, 10, 10, 10, 10, 10, 10, 0.1, 0.1, 0.1])*1
+diag_Q1 = np.array([10, 100, 1000])*100
 # diag_Q = np.array([10, 10, 10, 0.1, 0.1, 0.1, 10, 10, 10])*1
-diag_R = np.array([diag_Q[6],diag_Q[7],diag_Q[8]])
-Q = np.diag(diag_Q)
-R = np.diag(diag_R)
+diag_R1 = np.array([1])
+Q1 = np.diag(diag_Q1)
+R1 = np.diag(diag_R1)
 
-print(A_discrete.shape)
-print(B_discrete.shape)
-print(Q.shape)
-print(R.shape)
+diag_Q2 = np.array([10, 1, 0.01])*10000
+# diag_Q = np.array([10, 10, 10, 0.1, 0.1, 0.1, 10, 10, 10])*1
+diag_R2 = np.array([10000])
+Q2 = np.diag(diag_Q2)
+R2 = np.diag(diag_R2)
+
+diag_Q3 = np.array([10, 10, 0.1])*10000
+# diag_Q = np.array([10, 10, 10, 0.1, 0.1, 0.1, 10, 10, 10])*1
+diag_R3 = np.array([10000])
+Q3 = np.diag(diag_Q3)
+R3 = np.diag(diag_R3)
+
+
+K1, P1, eigenvalues1 = ctrl.lqr(A_0, B_0, Q1, R1)
+K2, P2, eigenvalues2 = ctrl.lqr(A_1, B_1, Q2, R2)
+K3, P3, eigenvalues3 = ctrl.lqr(A_2, B_2, Q3, R3)
+
+# K1, P1, eigenvalues1 = ctrl.dlqr(A_discrete_0, B_discrete_0, Q1, R1)
+# K2, P2, eigenvalues2 = ctrl.dlqr(A_discrete_1, B_discrete_1, Q2, R2)
+# K3, P3, eigenvalues3 = ctrl.dlqr(A_discrete_2, B_discrete_2, Q3, R3)
+
+K1_1 = np.array([K1[0][0],K1[0][1],K1[0][2]])
+# K1_1 = np.diag(K1_1)
+
+K2_1 = np.array([K2[0][0],K2[0][1],K2[0][2]])
+# K2_1 = np.diag(K2_1)
+
+K3_1 = np.array([K3[0][0],K3[0][1],K3[0][2]])
+# K3_1 = np.diag(K3_1)
+
+Kk1 = np.array([K1_1[0],0,0,K1_1[1],0,0,K1_1[2],0,0])
+Kk2 = np.array([0,K2_1[0],0,0,K2_1[1],0,0,K2_1[2],0])
+Kk3 = np.array([0,0,K3_1[0],0,0,K3_1[1],0,0,K3_1[2]])
+
+K = np.vstack((Kk1,Kk2,Kk3))
+asad,vect = np.linalg.eig(A-B@K)
+asad1,vect1 = np.linalg.eig(A_0-B_0@K1)
+asad2,vect2 = np.linalg.eig(A_1-B_1@K2)
+asad3,vect3 = np.linalg.eig(A_2-B_2@K3)
+# asad,vect = np.linalg.eig(A_discrete-B_discrete@K)
+# asad1,vect1 = np.linalg.eig(A_discrete_0-B_discrete_0@K1)
+# asad2,vect2 = np.linalg.eig(A_discrete_1-B_discrete_1@K2)
+# asad3,vect3 = np.linalg.eig(A_discrete_2-B_discrete_2@K3)
+
+q = np.array([0.0789,0.0941,0.0789,0.9893])
+w = np.array([0.0001, 0.0001, 0.0001])
+ws = np.array([0.00001, 0.00001, 0.00001])
+
+q0_real = [q[0]]
+q1_real = [q[1]]
+q2_real = [q[2]]
+q3_real = [q[3]]
+w0_real = [w[0]]
+w1_real = [w[1]]
+w2_real = [w[2]]
+w0s_real = [ws[0]]
+w1s_real = [ws[1]]
+w2s_real = [ws[2]]
+q_real = np.array([q0_real[-1],q1_real[-1],q2_real[-1],q3_real[-1]])
+w_body = np.array([w0_real[-1], w1_real[-1], w2_real[-1]])
+w_gyros = functions_03_rw.simulate_gyros_reading(w_body, 0,0)
+ws_real = np.array([w0s_real[-1], w1s_real[-1], w2s_real[-1]])
+x_real = np.hstack((np.transpose(q_real[:3]), np.transpose(w_gyros), np.transpose(ws_real)))
+
+for i in range(len(t)-1):
+    print(t[i+1])
+    # print(x_real)
+    # print(np.dot(-K*10,x_real))
+    
+
+    u_est = np.dot(-K,x_real)
+    # print(u_est)
+    u_est = functions_03_rw.torquer(u_est,100000)
+    
+    [xx_new_d, qq3_new_d] = functions_03_rw.mod_lineal_disc(
+        x_real, u_est, deltat, hh, A_discrete,B_discrete)
+    
+    # print(u_est)
+    # print(xx_new_d)
+    # input()
+    x_real = xx_new_d
+    w_gyros = functions_03_rw.simulate_gyros_reading(x_real[3:6],0,0)
+    ws_real = x_real[6:9]
+    q0_real.append(xx_new_d[0])
+    q1_real.append(xx_new_d[1])
+    q2_real.append(xx_new_d[2])
+    q3_real.append(qq3_new_d)
+    w0_real.append(w_gyros[0])
+    w1_real.append(w_gyros[1])
+    w2_real.append(w_gyros[2])
+    w0s_real.append(ws_real[0])
+    w1s_real.append(ws_real[1])
+    w2s_real.append(ws_real[2])
 
 
 
-# # Resolver la ecuación de Riccati
+fig0, axes0 = plt.subplots(nrows=1, ncols=2, figsize=(16, 4))
+
+axes0[0].plot(t, q0_real, label='q0 modelo')
+axes0[0].plot(t, q1_real, label='q1 modelo')
+axes0[0].plot(t, q2_real, label='q2 modelo')
+axes0[0].plot(t, q3_real, label='q3 modelo')
+axes0[0].set_xlabel('Tiempo [s]')
+axes0[0].set_ylabel('cuaternion [-]')
+axes0[0].legend()
+axes0[0].set_title('cuaterniones obtenidos por el modelo de control lineal discreto')
+axes0[0].grid()
+# axes0[0].set_ylim(-1, 1)  # Ajusta los límites en el eje Y
+
+axes0[1].plot(t, w0_real, label='w0 modelo')
+axes0[1].plot(t, w1_real, label='w1 modelo')
+axes0[1].plot(t, w2_real, label='w2 modelo')
+axes0[1].set_xlabel('Tiempo [s]')
+axes0[1].set_ylabel('velocidad angular [rad/s]')
+axes0[1].legend()
+axes0[1].set_title('velocidades angulares obtenidos por el modelo de control lineal discreto')
+axes0[1].grid()
+
+plt.tight_layout()
+plt.show()
+
+#%% Control LQR
+
+# # Definir las matrices Q y R del coste del LQR
+# diag_Q = np.array([1, 1, 1, 1, 1, 1, 0.1, 0.1, 0.1])*1
+# # diag_Q = np.array([10, 10, 10, 0.1, 0.1, 0.1, 10, 10, 10])*1
+# diag_R = np.array([diag_Q[6],diag_Q[7],diag_Q[8]])
+# Q = np.diag(diag_Q)
+# R = np.diag(diag_R)
+
+# print(A_discrete.shape)
+# print(B_discrete.shape)
+# print(Q.shape)
+# print(R.shape)
+
+
+
+# # # Resolver la ecuación de Riccati
+# # X,L,G=control.dare(A_discrete,B_discrete,Q,R)
 # P = solve_discrete_are(A_discrete, B_discrete, Q, R)
 
-# # Calcular la matriz de retroalimentación K
+# # # Calcular la matriz de retroalimentación K
 # K = np.linalg.inv(B_discrete.T @ P @ B_discrete + R) @ (B_discrete.T @ P @ A_discrete)
 
+# # P = solve_continuous_are(A,B,Q,R)
 
-K, P, eigenvalues = control.dlqr(A_discrete, B_discrete, Q, R)
+# # K, P, eigenvalues = control.lqr(A, B, Q, R)
+# # K, P, eigenvalues = control.dlqr(A_discrete, B_discrete, Q, R)
--- 03_kalman_lineal_mixto/untitled0.py
+++ 03_kalman_lineal_mixto/untitled0.py
@@ -1,20 +1,84 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 26 14:58:06 2024
+Created on Tue Dec 10 14:47:19 2024
 
 @author: nachi
 """
 
-diag_Q = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1])*1000
-diag_R = np.array([1,1,1])
-Q = np.diag(diag_Q)
-R = np.diag(diag_R)
-K, P, eigenvalues = control.dlqr(A_discrete, B_discrete, Q, R)
-Ad = A_discrete
-Bd = B_discrete
+import numpy as np
+import matplotlib.pyplot as plt
 
-TEST= Ad.T@P@Ad-P-Ad.T@P@Bd@np.linalg.inv((Bd.T@P@Bd+R))@Bd.T@P@Ad+Q
+# Parámetros del satélite
+I_body = np.diag([0.02, 0.02, 0.01])  # Tensor de inercia del satélite (kg·m²)
 
-print(TEST)
-# print(K)
-# np.max(abs(K))
+# Parámetros de las ruedas de reacción
+I_wheel = np.array([0.001, 0.001, 0.001])  # Momentos de inercia de las ruedas (kg·m²)
+wheel_speeds = np.array([0.0, 0.0, 0.0])   # Velocidades angulares iniciales de las ruedas (rad/s)
+
+# Velocidad angular inicial del satélite
+omega_body = np.array([0.0, 0.0, 0.0])  # rad/s
+
+# Tiempo de simulación
+dt = 0.01  # Paso de tiempo (s)
+t_total = 10  # Tiempo total de simulación (s)
+steps = int(t_total / dt)
+
+# Momentos aplicados a las ruedas (L^w) en Nm
+L_wheel = np.array([0.001, 0.0, 0.0])  # Momento aplicado solo en el eje x
+
+# Almacenar datos para graficar
+omega_body_history = []
+wheel_speeds_history = []
+
+# Simulación de la dinámica
+for _ in range(steps):
+    # Actualizar el momentum angular de las ruedas: dH^w = L^w * dt
+    dH_wheel = L_wheel * dt
+    wheel_speeds += dH_wheel / I_wheel
+
+    # Momentum angular de las ruedas H^w
+    H_wheel = I_wheel * wheel_speeds
+
+    # Momentum angular total: H = I_body * omega_body + H_wheel
+    H_total = I_body @ omega_body + H_wheel
+
+    # Calcular la nueva velocidad angular del cuerpo del satélite: omega_body = I_body^-1 * (H_total - H_wheel)
+    omega_body = np.linalg.inv(I_body) @ (H_total - H_wheel)
+
+    # Guardar datos para graficar
+    omega_body_history.append(omega_body.copy())
+    wheel_speeds_history.append(wheel_speeds.copy())
+
+# Convertir listas a arrays
+omega_body_history = np.array(omega_body_history)
+wheel_speeds_history = np.array(wheel_speeds_history)
+
+# Graficar resultados
+time = np.linspace(0, t_total, steps)
+
+plt.figure(figsize=(12, 6))
+
+# Velocidades angulares del satélite
+plt.subplot(2, 1, 1)
+plt.plot(time, omega_body_history[:, 0], label='ω_x (rad/s)')
+plt.plot(time, omega_body_history[:, 1], label='ω_y (rad/s)')
+plt.plot(time, omega_body_history[:, 2], label='ω_z (rad/s)')
+plt.title('Velocidades Angulares del Satélite')
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Velocidad Angular (rad/s)')
+plt.legend()
+plt.grid()
+
+# Velocidades angulares de las ruedas de reacción
+plt.subplot(2, 1, 2)
+plt.plot(time, wheel_speeds_history[:, 0], label='ω_wheel_x (rad/s)')
+plt.plot(time, wheel_speeds_history[:, 1], label='ω_wheel_y (rad/s)')
+plt.plot(time, wheel_speeds_history[:, 2], label='ω_wheel_z (rad/s)')
+plt.title('Velocidades Angulares de las Ruedas de Reacción')
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Velocidad Angular de Ruedas (rad/s)')
+plt.legend()
+plt.grid()
+
+plt.tight_layout()
+plt.show()
